d7aa2ea27d45b75c2f497afd202e6a36fd4dfc69
sbt330 <tessacareer@gmail.com>
2024-05-19 15:50:23 -0400

    Update bshipserver.cc

--- bshipserver.cc
+++ bshipserver.cc
@@ -8,20 +8,20 @@
 #include "time.h"
 
 const int DEFAULT_PORT = 30000;
+const int MESSAGE_SIZE = 250;
 
 ServerSocket theServer;
 FDSet fdset;
-const int MESSAGE_SIZE = 250;
 char message[MESSAGE_SIZE + 1];
-Socket *player1Client = 0;
-Socket *player2Client = 0;
+Socket *player1Client = nullptr;
+Socket *player2Client = nullptr;
 int playerSessId = 0;
 
 GameLogic gameLogic;
 
 void processRequests();
 void handleClientConnection();
-void handlePlayerRequest(int, Socket *);
+void handlePlayerRequest(int fd, Socket *clientSocket);
 int getSessionID();
 void closeConnections();
 
@@ -46,7 +46,104 @@ int main(int argc, char *argv[])
     }
 
     gameLogic.reset();
-    int val = getSessionID();
+    playerSessId = getSessionID();
+
+    // Main loop to handle client connections and requests
+    while (true)
+    {
+        fdset.set();
+
+        if (select(fdset.maxFD() + 1, fdset.getFDSet(), NULL, NULL, NULL) < 0)
+        {
+            perror("Error in select");
+            closeConnections();
+            exit(EXIT_FAILURE);
+        }
+
+        if (fdset.isSet(theServer.fd()))
+        {
+            handleClientConnection();
+        }
+
+        processRequests();
+    }
+
+    return 0;
+}
+
+void processRequests()
+{
+    if (player1Client != nullptr)
+    {
+        handlePlayerRequest(player1Client->fd(), player1Client);
+    }
+
+    if (player2Client != nullptr)
+    {
+        handlePlayerRequest(player2Client->fd(), player2Client);
+    }
+}
+
+void handleClientConnection()
+{
+    Socket *clientSocket = theServer.accept();
+    if (clientSocket == nullptr)
+    {
+        perror("Error accepting client connection");
+        closeConnections();
+        exit(EXIT_FAILURE);
+    }
+
+    int sessionId = clientSocket->recvInt();
+    if (sessionId != playerSessId)
+    {
+        printf("Invalid session ID. Connection rejected.\n");
+        delete clientSocket;
+        return;
+    }
+
+    printf("Player connected with session ID: %d\n", sessionId);
+
+    if (player1Client == nullptr)
+    {
+        player1Client = clientSocket;
+    }
+    else if (player2Client == nullptr)
+    {
+        player2Client = clientSocket;
+    }
+
+    fdset.addFD(clientSocket->fd());
+}
+
+void handlePlayerRequest(int fd, Socket *clientSocket)
+{
+    // Handle player's requests here
+}
+
+int getSessionID()
+{
+    srand(time(NULL));
+    return rand() % 100000 + 1;
+}
+
+void closeConnections()
+{
+    if (player1Client != nullptr)
+    {
+        player1Client->closeSocket();
+        delete player1Client;
+    }
+
+    if (player2Client != nullptr)
+    {
+        player2Client->closeSocket();
+        delete player2Client;
+    }
+
+    theServer.closeSocket();
+}
+
 
    
 
