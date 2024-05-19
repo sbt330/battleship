cf620219fe9163311afa1ca8143e4f0c89dfa3f7
sbt330 <tessacareer@gmail.com>
2024-05-19 15:10:05 -0400

    Update bshipserver.cc

--- bshipserver.cc
+++ bshipserver.cc
@@ -1,3 +1,7 @@
+/* gameserver.cc
+ *
+ */
+
 #include "socket.h"
 #include "select.h"
 #include <stdio.h>
@@ -8,7 +12,10 @@
 #include "netpacket.h"
 #include "time.h"
 
+//ask necaise about response codes and how to use them
+
 const int DEFAULT_PORT = 30000;
+
 
 ServerSocket theServer;  
 FDSet fdset;
@@ -18,6 +25,8 @@ char message[MESSAGE_SIZE+1];
 Socket *player1Client = 0;
 Socket *player2Client = 0;
 int playerSessId = 0;
+
+
 
 GameLogic gameLogic;
 
@@ -36,16 +45,17 @@ int main(int argc, char *argv[])
    /* Get the port number. */
   int portNum;
   
-  if (argc < 2) 
+  if( argc < 2 ) 
     portNum = DEFAULT_PORT;
   else
     portNum = atoi(argv[1]);
   
    /* Bind the server's listening socket to the given port. */
-  if (theServer.bind(portNum)) {
+  if( theServer.bind(portNum) ) {
     printf("Server bound to port #%d\n", portNum);
     fdset.addFD(theServer.fd());
-  } else {
+  }
+  else {
     printf("Error: can not bind to port #%d\n", portNum);
     exit(1);
   }
@@ -53,7 +63,7 @@ int main(int argc, char *argv[])
    /* Initialize the game logic. */
   gameLogic.reset();
   
-  /* Gets the random player ID */
+  /* Gets the random player ID*/
   val = getSessionID();
   
    /* Process client requests. */
@@ -61,25 +71,25 @@ int main(int argc, char *argv[])
 }
 
 
- /* Process client requests, connections, and messages, after starting the server. */
+ /* Process client requests, connections and messages, after starting the server. */
 void processRequests()
 {
-  while (1) {
+  while(1) {
      /* Wait for input on the input streams in the set. */
     int *activeSet = fdset.select();
   
      /* Handle input from one of the sockets. */
     int i = 0; 
-    while (activeSet[i] >= 0) {
+    while(activeSet[i] >= 0) {
       int fd = activeSet[i];
       i++;
       
-      if (fd == theServer.fd()) 
+      if(fd == theServer.fd()) 
         handleClientConnection();
       else {
-        if (player1Client && fd == player1Client->fd())
+        if(player1Client && fd == player1Client->fd())
           handlePlayerRequest(1, player1Client);
-        else if (player2Client && fd == player2Client->fd())
+        else if(player2Client && fd == player2Client->fd())
           handlePlayerRequest(2, player2Client);
       }
     }    
@@ -87,49 +97,74 @@ void processRequests()
 }
 
 /* Handle a player request, which includes both placing of the ships and 
-    taking shots at the opponents' ships. */
-void handlePlayerRequest(int player, Socket *clientSocket) {
-    PlayerRequest request;
-    PlayerResponse response;
+    taking shots at the opponents ships. */
+void handlePlayerRequest(int player, Socket *client)
+{
+  PlayerRequest request;
+  int result = client->recv(&request, sizeof(request));
 
-    // Read the player request.
-    clientSocket->recv(&request, sizeof(request));
-
-    // Process the request based on the operation code.
-    switch (request.op) {
-    case 1: // Place a ship.
-        response.code = gameLogic.placeShip(player, request.row, request.col, request.dir, request.shipType);
-        break;
-
-    case 2: // Fire a shot.
-        response.code = gameLogic.fireShot(request.row, request.col);
-        break;
-
-    default:
-        response.code = ERR_INVALID_CODE;
-        break;
+   /* If a player closes its connection, end the game and close all sockets. */
+  if(result == 0) 
+    closeConnections();
+  
+   /* Build the response. */
+  PlayerResponse response = {0, 0, 0, 0, 0};
+  
+  if(gameLogic.inSetupMode()) {
+  	printf("In setup mode.\n");
+  	
+    // handle the requests made during setup mode.
+    // when both players have placed all of their ships, then start the game.
+    if (request.op == 1)
+    {
+    	printf("Placing ship %d for player %d at (%d, %d)\n", request.shipType, player, request.row, request.col);
+    	
+    	/*
+    	bool placeShip = gameLogic.placeShip(player, request.shipType, request.row, request.col, request.dir);
+    	
+    	if (placeShip)
+    	{
+    		response.code = 0;
+    	}
+    	else
+    	{
+    		response.code = -11;
+    	}
+    	*/
+    	response.code = 0;
+    	client->send(&response, sizeof(response));
+    	
+    	if (gameLogic.numShips(1) == 5 && gameLogic.numShips(2) == 5)
+    	{
+    		gameLogic.startGame();
+    		printf("Game has begun.\n");
+    	}
     }
-
-    // Add missing turn and ship count information to the response.
-    response.turn = gameLogic.WhoseTurn;   // Assuming WhoseTurn is public, otherwise use a method to get it.
-    response.numShipsPlayer1 = gameLogic.numShips(1);
-    response.numShipsPlayer2 = gameLogic.numShips(2);
-
-    // Send the response to the client.
-    clientSocket->send(&response, sizeof(response));
+  }
+  
+  else if(gameLogic.inPlayMode()) {
+  	  printf("In play mode.\n");
+  	  processRequests(); 
+    
+  }  
 }
-/* Cleanly closes the connections of both players and resets the game logic. */
+
+
+ /* Cleanly closes the connections of both players and resets the game logic. 
+    This can be usefulf in the situation where the client of one player shuts 
+    down before the game ends. In that case, the sockets of both players 
+    have to be closed and deleted and the game reset. */
 void closeConnections()
 {
   printf("**A player closed their connection, end the game!\n");
   
-  if (player1Client) {
+  if(player1Client) {
     fdset.removeFD(player1Client->fd());    
     player1Client->close();
     delete player1Client;
     player1Client = 0;
   }
-  if (player2Client) {
+  if(player2Client) {
     fdset.removeFD(player2Client->fd());
     player2Client->close();
     delete player2Client;
@@ -139,97 +174,125 @@ void closeConnections()
   gameLogic.reset();  
 }
 
-/* Functions used with handleClientConnection */
+
+ /* Functions used with handleClientConnection */
 void connectPlayer1(Socket *);
 void connectPlayer2(Socket *, int);
 
-/* Manages a client connection as either player 1 or player 2. */ 
+
+ /* Manages a client connection as either player 1 or player 2. */ 
 void handleClientConnection()
 {
+
    /* Accept the connection. */
   Socket *client = theServer.accept();
   printf("Client connected from: %s on port #%d\n", 
-          client->address(), client->port());
+		  client->address(), client->port());
   
    /* Read the connection request packet. */
   ConnRequest request;
   InitResponse response = {0, 0};
   
   int result = client->recv(&request, sizeof(request));
-  if (result == 0) {
-    printf("Connection: client closed the connection before sending request.\n");
-    client->close();
-    delete client;
-  } else {
-    if (request.code == 1)
-      connectPlayer1(client);
-    else if (request.code == 2)
-      connectPlayer2(client, request.sessionId);
-    else {
-      response.code = ERR_INVALID_CODE;
-      client->send(&response, sizeof(response));
-      client->close();   
-      printf("Connection: invalid connection request.\n");
-      delete client;      
-    }
+  if(result == 0) {
+	printf("Connection: client closed the connection before sending request.\n");
+	client->close();
+	delete client;
+  }
+  else {
+	if(request.code == 1)
+	  connectPlayer1(client);
+	else if(request.code == 2)
+	  connectPlayer2(client, request.sessionId);
+	else {
+	  response.code = ERR_INVALID_CODE;
+	  client->send(&response, sizeof(response));
+	  client->close();   
+	  printf("Connection: invalid connection request.\n");
+	  delete client;      
+	}
   }
 }
 
-/* Manages the connection for player 1, which must return a session id. */
+
+
+ /* Manages the connection for player 1, which must return a session id. */
 void connectPlayer1(Socket *client)
 {
-  InitResponse response = {0, 0};
-  if (player1Client) {
-    response.code = -1;
-    client->send(&response, sizeof(response));
-    client->close();
-    delete client;
-    printf("Error. Invalid number of players.\n");
-    exit(1);
-  } else {
-    player1Client = client;
-    fdset.addFD(client->fd());
-    
-    response.playerSessId = val;
-    strcpy(message, "Player 1 is connected.\n");
-    printf(message);
-    client->send(&response, sizeof(response));
+	 InitResponse response = {0, 0};
+	if (player1Client)
+  {
+  	   response.code = -1;
+	  client->send(&response, sizeof(response));
+	  client->close();
+	  delete client;
+	  printf("Error. Invalid number of players.\n");
+	    exit(1);
+		 
   }
+  else
+  {
+	  player1Client = client;
+	  fdset.addFD(client->fd());
+	
+	  response.playerSessId = val;
+	  strcpy(message, "Player 1 is connected.\n");
+	  printf(message);
+	  client->send(&response, sizeof(response));
+	  }
 }
 
-/* Manages the connection for player 2, which must verify the session id. */
+
+ /* Manages the connection for player 2, which must verify the session id. */
 void connectPlayer2(Socket *client, int sessId)
 {
+	  
   InitResponse response = {0, 0};
-  if (player2Client) {
-    response.code = -1;
-    client->send(&response, sizeof(response));
-    client->close();
-    delete client;
-    printf("Error. Invalid number of players.\n");
-    exit(1);
-  } else {
-    if (val == sessId) {
-      player2Client = client;
-      fdset.addFD(client->fd());
-     
-      strcpy(message, "Player 2 is connected.\n");
-      printf(message);
-      client->send(&response, sizeof(response));
-    } else {
-      printf("Invalid session ID.\n");
-      response.code = ERR_INVALID_CODE;
-      client->send(&response, sizeof(response));
-      client->close();
-      delete client;
-    }
+	if (player2Client)
+  {
+  	   response.code = -1;
+	  client->send(&response, sizeof(response));
+	  client->close();
+	  delete client;
+	  printf("Error. Invalid number of players.\n");
+	  exit(1);
+		  
+  }
+  else
+  {
+
+  
+  //Match session Ids to start game
+  if (val == sessId)
+  {
+  	  player2Client = client;
+  	  fdset.addFD(client->fd());
+  	 
+  	  strcpy(message, "Player 2 is connected.\n");
+  	  printf(message);
+  	  client->send(&response, sizeof(response));
+  
+  }
+  else
+  {
+  	  printf("Invalid session ID.\n)");
+  	  response.code = ERR_INVALID_CODE;
+  	  client->send(&response, sizeof(response));
+  	  client->close();
+  	  delete client;
+  	  
+  } 
   }
 }
 
 int getSessionID()
 {
-  srand(time(0));
-  int val = rand() % 100000 + 1000;
-  printf("Sess ID: %d\n", val);
-  return val;
+	srand(time(0));
+	int val = rand() % 100000 + 1000;
+	printf("Sess ID: %d\n", val);
+	return val;
 }
+
+
+
+
